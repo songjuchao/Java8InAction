d24a4ec3b8e88d2968d59c5b08e03f2940479639
juchaosong <juchaosong@sohu-inc.com>
2017-11-01 14:58:08 +0800

    add chp5

---
+++ src/lambdainaction/chap5/Practice.java
@@ -0,0 +1,46 @@
+package lambdainaction.chap5;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Practice {
+    public static void main(String... args) {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario", "Milan");
+        Trader alan = new Trader("Alan", "Cambridge");
+        Trader brian = new Trader("Brian", "Cambridge");
+
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
+
+        transactions.stream().
+                filter(transaction -> transaction.getYear() == 2011)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .forEach(transaction -> System.out.println(transaction));
+
+        transactions.stream().map(Transaction::getTrader)
+                .distinct()
+                .map(Trader::getCity)
+                .distinct()
+                .forEach(trader -> System.out.println(trader));
+
+        transactions.stream().map(Transaction::getTrader)
+                .distinct()
+                .filter(trader -> trader.getCity().equals("Cambridge"))
+                .sorted(Comparator.comparing(Trader::getName))
+                .forEach(trader -> System.out.println(trader));
+
+        transactions.stream().map(Transaction::getTrader)
+                .map(Trader::getName)
+                .distinct()
+                .sorted()
+                .forEach(trader -> System.out.println(trader));
+    }
+}
